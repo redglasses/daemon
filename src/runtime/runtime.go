eb989d87548b767d08523cde1a4c94fef7583323
Kenny Cheng <kenny97821419@gmail.com>
2013-04-28 11:01:15 +0800

    initial version of the runtime system.

---
+++ src/runtime/runtime.go
@@ -0,0 +1,35 @@
+package runtime
+
+import (
+	"net/http"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type Condition func() bool
+type Machine interface {
+	Init()
+	InState(string) bool
+	FireEvent(string) bool
+	ValidateConditions()
+}
+
+var MachineRegistry map[string]Machine
+var Bind = ":" + strconv.Itoa(os.Getpid())
+
+func AddMachine(name string, m Machine) {
+	MachineRegistry[name] = m
+}
+
+func Run() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		component := strings.Split(strings.Trim(r.URL.Path, "/"), "/")
+		if m, ok := MachineRegistry[component[0]]; ok {
+			m.FireEvent(component[0])
+		}
+	})
+	http.ListenAndServe(Bind, nil)
+
+	os.Exit(0)
+}
